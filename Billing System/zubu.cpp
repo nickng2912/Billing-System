a0c7d44c82c0a5aa805a9de00ef0a7da93f4a8cb
nickng2912 <nickng2912@gmail.com>
2023-01-08 03:00:32 -0800

    Finish Menu, admin, Customers, Add, Edit. Still need to work on the rest Stop at 47:12

---
+++ Billing System/zubu.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+#include <fstream>
+#include "zubu.h"
+using namespace std; 
+
+void Zubu::menu() {
+
+	start: 
+	int choice = 0; 
+	string email; 
+	string password; 
+
+	cout << "-------------------------------------------\n";
+	cout << "\t Zubu Ramen Main Menu" << endl; 
+	cout << endl << "1) Administator" << endl; 
+	cout << endl << "2) Buyer" << endl; 
+	cout << endl << "3) Exit" << endl; 
+	cout << endl << "Please selet: ";
+	cin >> choice;
+
+	if (choice == 1) {
+		cout << endl << "Please Login" << endl; 
+		cout << endl << "Email: "; 
+		cin >> email; 
+		cout << endl << "Password: ";
+		cin >> password;
+
+		if (email == "trung.ng2912@gmail" && password == "123456789")
+		{
+			administrator();
+		}
+		else {
+			cout << "Invalid Email/Password" << endl;
+		}
+
+	}
+	else if (choice == 2) {
+		customers(); 
+	}
+	else if (choice == 3) {
+		exit(0);
+	}
+	else {
+		cout << "Please select from the following options!" << endl; 
+	}
+	goto start;
+}
+
+void Zubu::administrator() {
+
+	start:
+	int choice = 0;
+	
+	cout << "-------------------------------------------\n";
+	cout << "\t Administrator Main Menu" << endl;
+	cout << endl << "1) Add a Product " << endl; 
+	cout << endl << "2) Modify a Product" << endl; 
+	cout << endl << "3) Remove a Product" << endl; 
+	cout << endl << "4) Back to Main Menu" << endl; 
+	cout << endl << "Please selet: ";
+	cin >> choice;
+
+	if (choice == 1) {
+		add(); 
+	}
+	else if (choice == 2) {
+		edit(); 
+	}
+	else if (choice == 3) {
+		remove(); 
+	}
+	else if (choice == 4) {
+		menu();
+	}
+	else {
+		cout << "Please select from the following options!" << endl;
+	}
+
+	goto start;
+
+}
+
+void Zubu::customers() {
+	
+	start:
+	int choice = 0; 
+
+	cout << "-------------------------------------------\n";
+	cout << "\t Customers Main Menu" << endl;
+	cout << endl << "1) Buy a Product " << endl;
+	cout << endl << "2) Back to Main Menu " << endl;
+	cout << endl << "Please selet: ";
+	cin >> choice; 
+
+	if (choice == 1) {
+		printReceipt();
+	}
+	else if (choice == 2) {
+		menu();
+	}
+	else {
+		cout << "Please select from the following options!" << endl;
+	}
+	goto start;
+
+}
+
+void Zubu::add() {
+
+	start:
+	fstream value; 
+	int newCode; 
+	int token = 0; 
+	float newPrice;
+	float newDiscount; 
+	string newName;
+
+	cout << "-------------------------------------------\n";
+	cout << "\t Add NEW Product" << endl;
+	cout << "Product Code of the Product: "; 
+	cin >> code; 
+	cout << endl << "Name of the Product: ";
+	cin >> productName; 
+	cout << endl << "Price of the Product: ";
+	cin >> price; 
+	cout << endl << "Discount on the Product: "; 
+	cin >> discount; 
+
+	value.open("database.txt", ios::in);
+
+	if (!value) {
+		value.open("database.txt", ios::app | ios::out);
+		value << " " << code << " " << productName << " " << price << " " << discount << endl;
+		value.close();
+	}
+	else {
+		value >> newCode >> newName >> newPrice >> newDiscount; 
+
+		while(!value.eof()) {
+			if (newCode == code) {
+				token++;
+			} 
+			value >> newCode >> newName >> newPrice >> newDiscount;
+		}
+		value.close();
+	}
+
+	if (token == 1) {
+		goto start;
+	}
+	else {
+		value.open("database.txt", ios::app | ios::out);
+		value << " " << code << " " << productName << " " << price << " " << discount << endl;
+		value.close();
+
+	}
+
+	cout << endl << "Product Inserted to Stock!" << endl; 
+
+}
+
+void Zubu::edit() {
+
+	fstream data1, data2;
+	int productKey; 
+	int token = 0; 
+	int newCode; 
+	float newPrice; 
+	float newDiscount;
+	string newName; 
+
+	cout << "-------------------------------------------\n";
+	cout << "\t Edit a Product Record" << endl;
+	cout << "Product Code of the Product: "; 
+	cin >> code; 
+	
+	data1.open("database.txt", ios::in);
+
+	if (!data1) {
+		cout << endl << "File Does Not Exist!" << endl; 
+	}
+	else {
+		data2.open("databaseTEMP.txt", ios::app | ios::out);
+
+		data1 >> code >> productName >> price >> discount; 
+		while (!data1.eof()) {
+			if (productKey == code) {
+				cout << endl << "\t New Product Code: "; 
+				cin >> newCode; 
+				cout << endl << "\t Name of the Product: ";
+				cin >> newName;
+				cout << endl << "\t Price of the Product: ";
+				cin >> newPrice; 
+				cout << endl << "\t Discount of the Product: ";
+				cin >> newDiscount;
+
+				data2 << " " << newCode << " " << newName << " " << newPrice << " " << newDiscount << endl;
+				
+				cout << "Record Edited!" << endl; 
+				token++;
+			}
+			else {
+				data2 << " " << code << " " << productName << " " << price << " " << discount << endl; 
+			}
+			data1 >> code >> productName >> price >> discount; 
+		}
+		data1.close(); 
+		data2.close(); 
+
+		remove("database.txt");
+		rename("databaseTEMP.txt", "databgase.txt");
+
+		if (token == 0) {
+			cout << endl << "Record Not Found!" << endl; 
+		}
+	}
+}
---
+++ Billing System/zubu.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Zubu {
+public:
+	void menu();
+	void administrator();
+
+	void customers();
+	void add();
+	void edit();
+	void remove();
+	void list();
+	void printReceipt();
+
+
+private:
+	int code;
+	float price;
+	float discount;
+	string productName;
+};
+
